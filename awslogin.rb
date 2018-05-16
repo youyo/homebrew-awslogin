717ca3eb7925b68968159f8f9fe2759d2a8ff5e2
youyo <1003ni2@gmail.com>
2018-05-16 15:01:55 +0900

    bugfix

--- awslogin.rb
+++ awslogin.rb
@@ -2,7 +2,7 @@ class Awslogin < Formula
   desc "AWS management console login tool."
   homepage "https://github.com/youyo/awslogin"
   url "https://github.com/youyo/awslogin/releases/download/1.1.5/awslogin"
-  sha256 "bd6d0eab432c09fe8d55dfd4efadf9d49a2bf89b495fb834615ca63f8881320b"
+  sha256 "1f32f1e23d14eae0d2fcb0528c5fe189ce238c96ecf8d03d5e739673d8f7996e"
   depends_on "peco" => :run
 
   def install
