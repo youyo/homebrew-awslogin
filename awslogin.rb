ffd4206ac46147f04baf2d2c0029e395b4295fd1
youyo <1003ni2@gmail.com>
2018-05-22 14:55:33 +0000

    [skip ci] bump up 1.1.9

--- awslogin.rb
+++ awslogin.rb
@@ -2,7 +2,7 @@ class Awslogin < Formula
   desc "AWS management console login tool."
   homepage "https://github.com/youyo/awslogin"
   url "https://github.com/youyo/awslogin/releases/download/1.1.9/awslogin"
-  sha256 ""
+  sha256 "9a8747afb7443d9ef885740c537dfbc6878a6b3c35568bcf19585fbe6efa0fb8"
   depends_on "peco"
 
   def install
